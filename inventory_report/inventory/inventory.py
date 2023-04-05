2d9b372cbafc9ce0ae96824fc8c7d528e3a8a93b
Manuela França <manuelaalves26@gmail.com>
2023-04-04 21:35:13 -0300

    feat: gera relatórios json e xml.

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -8,13 +10,44 @@ class Inventory:
         pass
 
     @classmethod
-    def import_data(cls, file_path: str, type_report: str):
-        with open(file_path, "r") as file:
-            reader = csv.DictReader(file)
-            data = []
-            for row in reader:
-                data.append(row)
-            if type_report == "simples":
-                return SimpleReport.generate(data)
-            else:
-                return CompleteReport.generate(data)
+    def import_data(cls, file_path: str, report_type: str):
+        if file_path.endswith(".csv"):
+            with open(file_path, "r") as file:
+                reader = csv.DictReader(file)
+                data = [row for row in reader]
+                return (
+                    SimpleReport.generate(data)
+                    if report_type == "simples"
+                    else CompleteReport.generate(data)
+                )
+
+        elif file_path.endswith(".json"):
+            with open(file_path) as file:
+                data = json.load(file)
+                return (
+                    SimpleReport.generate(data)
+                    if report_type == "simples"
+                    else CompleteReport.generate(data)
+                )
+
+        elif file_path.endswith(".xml"):
+            with open(file_path) as file:
+                tree = ET.parse(file)
+                root = tree.getroot()
+                data = []
+
+                product = {}
+                for element in root.iter():
+                    if "\n" not in element.text:
+                        product[element.tag] = element.text
+                    if element.tag == "instrucoes_de_armazenamento":
+                        data.append(product)
+                        product = {}
+                return (
+                    SimpleReport.generate(data)
+                    if report_type == "simples"
+                    else CompleteReport.generate(data)
+                )
+
+        else:
+            raise ValueError("Invalid file format")
